18257ebe32f61b70670a5333535018596da5fbdc
thinhbui-303 <quangthinhbqt303@gmail.com>
2026-04-22 15:24:36 +0700

    initial commit

---
+++ enotes-api-service/src/test/java/com/thinhbqt/enotes_api_service/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinhbqt.enotes_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
