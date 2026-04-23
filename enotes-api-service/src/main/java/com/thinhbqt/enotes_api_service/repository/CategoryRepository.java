8c8a0235f8d4b0416729824a5d4deb8536df5cc4
thinhbui-303 <quangthinhbqt303@gmail.com>
2026-04-23 14:33:27 +0700

    implement save and get category

---
+++ enotes-api-service/src/main/java/com/thinhbqt/enotes_api_service/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.thinhbqt.enotes_api_service.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thinhbqt.enotes_api_service.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+    
+}
---
+++ enotes-api-service/src/main/java/com/thinhbqt/enotes_api_service/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.thinhbqt.enotes_api_service.service;
+
+import java.util.List;
+
+import com.thinhbqt.enotes_api_service.dto.CategoryDto;
+import com.thinhbqt.enotes_api_service.dto.CategoryResponse;
+
+
+public interface CategoryService {
+
+    public Boolean saveCategory(CategoryDto categoryDto);
+
+    public List<CategoryResponse> getAllCategory();
+}
